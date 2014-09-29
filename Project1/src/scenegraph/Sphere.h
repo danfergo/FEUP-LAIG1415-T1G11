393359aaea33cbe3507bb0f3d14cc1b076a2b68e
Nelson <paracontas987@hotmail.com>
2014-09-29 15:58:12 +0100

    Sphere no trabalho.

---
+++ Project1/src/scenegraph/Sphere.h
@@ -0,0 +1,25 @@
+#ifndef MYSEMISPHERE_X
+#define MYSEMISPHERE_X
+
+#include "CGFobject.h"
+#include "CGFappearance.h"
+#include "CGFlight.h"
+#include "Point.h"
+
+
+class Sphere: public CGFobject{
+	private:
+		float angle_a, angle_b;
+		unsigned stacks, slices;
+		Point3d ** dots;
+		
+	public:
+		Sphere(unsigned stacks, unsigned slices);
+		void draw();
+		~Sphere();
+};
+
+
+
+
+#endif
