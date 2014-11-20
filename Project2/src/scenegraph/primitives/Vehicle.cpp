8d84edb7517b7cee5e724eaf6563a7fc432260b5
Nelson <paracontas987@hotmail.com>
2014-11-20 01:37:25 +0000

    Vehicle
    Vehicle done

--- Project2/src/scenegraph/primitives/Vehicle.cpp
+++ Project2/src/scenegraph/primitives/Vehicle.cpp
@@ -3,14 +3,87 @@
 
 Vehicle::Vehicle(void)
 {
+	this->quadric =  gluNewQuadric();
+	corpo = new Sphere(this->quadric, 3, 10, 10);
+	cauda = new Cylinder( this->quadric,  0.4, 0.2, 12.1, 8, 8);
+	heliceG = new Cylinder( this->quadric,  0.1, 0.25, 8, 4, 4);
+	heliceS = new Cylinder( this->quadric,  0.07, 0.15, 2, 4, 4);
+	traseira = new Cylinder( this->quadric,  0.07, 0.15, 0.5, 4, 4);
 }
 
 
 Vehicle::~Vehicle(void)
-{
+{/*
+	delete corpo;
+	delete cauda;
+	delete heliceG;
+	delete heliceS;
+	delete traseira;
+	delete quadric;*/
 }
 
 
 void Vehicle::draw(Texture * texture){
+	
+	glPushMatrix();
+		glRotatef(90, 0,1,0);
+		glScalef(1.5,1,1);
+		corpo->draw(texture);
+	glPopMatrix();
 
+	glPushMatrix();
+		cauda->draw(texture);
+	glPopMatrix();
+	
+	glPushMatrix();
+		glTranslatef(0,3.2,0);
+
+		glPushMatrix();
+		glRotatef(90, 0,1,0);
+		heliceG->draw(texture);
+		glPopMatrix();
+		
+		glPushMatrix();
+		glRotatef(180, 0,1,0);
+		heliceG->draw(texture);
+		glPopMatrix();
+		
+		glPushMatrix();
+		glRotatef(270, 0,1,0);
+		heliceG->draw(texture);
+		glPopMatrix();
+		
+		glPushMatrix();
+		heliceG->draw(texture);
+		glPopMatrix();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(-0.55,0,12);
+		glRotatef(90,0,0,1);
+
+		glPushMatrix();
+		glRotatef(90, 1,0,0);
+		traseira->draw(texture);
+		glPopMatrix();
+
+		glPushMatrix();
+		glRotatef(90, 0,1,0);
+		heliceS->draw(texture);
+		glPopMatrix();
+
+		glPushMatrix();
+		glRotatef(180, 0,1,0);
+		heliceS->draw(texture);
+		glPopMatrix();
+		
+		glPushMatrix();
+		glRotatef(270, 0,1,0);
+		heliceS->draw(texture);
+		glPopMatrix();
+		
+		glPushMatrix();
+		heliceS->draw(texture);
+		glPopMatrix();
+	glPopMatrix();
 }
